cf55a7221d75bcf34d5507587e38f4731dc2cfb3
wkd37 <wkd37@DESKTOP-30K9SAV>
2023-07-13 21:01:56 +0900

    과제1

--- HelloWorld/src/co/yedam/ConditionExe2.java
+++ HelloWorld/src/co/yedam/ConditionExe2.java
@@ -9,12 +9,17 @@ public class ConditionExe2 {
 			System.out.println("메뉴를 선택하세요(숫자를 입력)>> ");
 			System.out.println("1.로그인 2.로그아웃 3.회원가입 4.종료");
 			String menu = scn.nextLine();
-			
+			int menus = Integer.parseInt(menu);
 			//switch, if elseif else
-			// "1"->equals, 1-> ==
+			// "1"->equals, 1-> ==			
+			switch(menus) {
+			case 1: login(); break;
+			case 2: logout(); break;
+			case 3: signUp(); break;
+			}if(menus == 4) {exit(); break;}
+
 		}
 		
-		System.out.println("end of prog");
 		
 	} // end of main()
 	
@@ -26,5 +31,9 @@ public class ConditionExe2 {
 	}
 	public static void signUp() {
 		System.out.println("회원가입메뉴입니다");
+	}
+	public static void exit() {
+		System.out.println("end of prog");
+		int menus = 0;
 	}
 }
